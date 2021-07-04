e6c857a97ebea64474eb93571434306243a160cf
sergiocardenasg <sergiocardenas@utexas.edu>
2021-07-03 20:30:25 -0500

    solution

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+var multiply = num1 * num2;
+
+var random = Math.ceil(Math.random() + 1);
+
+let num3 = 9;
+let num4 = 5;
+
+var mod = num3 % num4;
+
+var max = Math.max(20)
+
